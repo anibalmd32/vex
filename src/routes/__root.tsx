0d395ebf93f1536526e3d7fcf73bb822f79180e8
= <=>
2026-02-26 11:04:42 -0400

    config root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,34 @@
+import { Box, Container } from "@mui/material";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import Dock from "../components/AppNavigation/Dock";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return <Outlet />;
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        pt: 2,
+      }}
+    >
+      <Box>
+        <Outlet />
+      </Box>
+      <Box
+        sx={{
+          px: 2,
+        }}
+      >
+        <Dock />
+      </Box>
+    </Container>
+  );
 }
